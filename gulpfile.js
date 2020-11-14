8e4c8fa27d0924a70b7eda59974b2e7b0cce9dc8
vilu <vilu85@users.noreply.github.com>
2020-11-14 13:49:45 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,158 @@
+const gulp = require('gulp');
+const terser = require('gulp-terser');
+const autoprefixer = require('gulp-autoprefixer');
+const csso = require('gulp-csso');
+const htmlmin = require('gulp-htmlmin');
+const del = require('del');
+const zip = require('gulp-zip');
+const javascriptObfuscator = require('gulp-javascript-obfuscator');
+
+var jsSrc = './src/**/*.js';
+var cssSrc = './src/**/*.css';
+var htmlSrc = './src/**/*.html';
+
+const AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+gulp.task('styles', function () {
+  return gulp.src( cssSrc )
+                // Auto-prefix css styles for cross browser compatibility
+                //.pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+                .pipe(autoprefixer())
+                // Minify the file
+                .pipe(csso())
+                // Output
+                .pipe(gulp.dest('./build'));
+});
+
+gulp.task( 'scripts', function() {
+  return  gulp.src( jsSrc )
+                .pipe( terser() )
+                .pipe( gulp.dest( './build' ) );
+});
+
+// Gulp task to minify HTML files
+gulp.task('html', function() {
+  return gulp.src( htmlSrc )
+                .pipe(htmlmin({
+                  collapseWhitespace: true,
+                  removeComments: true
+                }))
+                .pipe(gulp.dest('./build'));
+});
+
+// gulp.task( 'styles', function() {
+//   return  gulp.src( sassSrc )
+//                 .pipe( sass( { outputStyle: 'compressed' } ) )
+//                 .pipe( gulp.dest( './build/css' ) );
+// });
+
+gulp.task('zip', function () {
+  return gulp.src('./src/**')
+                .pipe( zip('AspaTukiChatPlugin_node.zip') )
+                .pipe(gulp.dest('./build'));
+});
+
+var files = [
+  './src/**/*.html',
+  './src/**/*.js',
+  './src/**/*.css',
+  './src/**/*.png',
+  './src/**/*.tt',
+  './src/**/*.pm',
+  './src/**/*.svg'
+];
+gulp.task('buildSources', function() {
+  return gulp.src(files)
+                .pipe(gulp.dest('./build'));
+});
+
+gulp.task('obfuscate', function() {
+  return gulp.src( jsSrc )
+                .pipe(javascriptObfuscator())
+                .pipe(gulp.dest('./build'));
+});
+
+gulp.task( 'automate', function() {
+  gulp.watch( [ jsSrc, cssSrc, htmlSrc ], gulp.series('scripts', 'styles', 'html'));
+});
+
+// gulp.task('bump', function () {
+//   /// <summary>
+//   /// It bumps revisions
+//   /// Usage:
+//   /// 1. gulp bump : bumps the package.json and bower.json to the next minor revision.
+//   ///   i.e. from 0.1.1 to 0.1.2
+//   /// 2. gulp bump --version 1.1.1 : bumps/sets the package.json and bower.json to the 
+//   ///    specified revision.
+//   /// 3. gulp bump --type major       : bumps 1.0.0 
+//   ///    gulp bump --type minor       : bumps 0.1.0
+//   ///    gulp bump --type patch       : bumps 0.0.2
+//   ///    gulp bump --type prerelease  : bumps 0.0.1-2
+//   /// </summary>
+
+//   var type = args.type;
+//   var version = args.version;
+//   var options = {};
+//   if (version) {
+//       options.version = version;
+//       msg += ' to ' + version;
+//   } else {
+//       options.type = type;
+//       msg += ' for a ' + type;
+//   }
+
+//   return gulp.src(['./package.json'])
+//                 .pipe(bump(options))
+//                 .pipe(gulp.dest('./'));
+// });
+
+// gulp.task('increment-version', function(){
+//   //docString is the file from which you will get your constant string
+//   var docString = fs.readFileSync('./someFolder/constants.js', 'utf8');
+
+//   //The code below gets your semantic v# from docString
+//   var versionNumPattern=/'someTextPreceedingVNumber', '(.*)'/; //This is just a regEx with a capture group for version number
+//   var vNumRexEx = new RegExp(versionNumPattern);
+//   var oldVersionNumber = (vNumRexEx.exec(docString))[1]; //This gets the captured group
+
+//   //...Split the version number string into elements so you can bump the one you want
+//   var versionParts = oldVersionNumber.split('.');
+//   var vArray = {
+//       vMajor : versionParts[0],
+//       vMinor : versionParts[1],
+//       vPatch : versionParts[2]
+//   };
+
+//   vArray.vPatch = parseFloat(vArray.vPatch) + 1;
+//   var periodString = ".";
+
+//   var newVersionNumber = vArray.vMajor + periodString +
+//                          vArray.vMinor+ periodString +
+//                          vArray.vPatch;
+
+//   gulp.src(['./someFolder/constants.js'])
+//       .pipe(replace(/'someTextPreceedingVNumber', '(.*)'/g, newVersionNumber))
+//       .pipe(gulp.dest('./someFolder/'));
+// });
+
+// Clean output directory
+gulp.task('clean', () => del(['build']));
+
+// Gulp task to minify all files
+gulp.task( 'minifyAll', gulp.series('styles','scripts','html'));
+ 
+gulp.task( 'default', gulp.series('zip'));
+
+gulp.task( 'build', gulp.series( 'zip' ));
+
+//gulp.task( 'build release', gulp.series( 'buildSources', 'minifyAll', 'obfuscate', 'zip' ));
---
+++ src/index.js
@@ -0,0 +1,199 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+var registry = new Map();
+const activeUsers = new Set();
+
+server.listen(port, () => {
+  console.log('AspaTukiChat Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Chatroom
+
+var numUsers = 0;
+
+io.on('connection', (socket) => {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    if(data.length > 1 && data.charAt(0) == "/") {
+      parseCommand(data);
+    } else {
+      socket.broadcast.emit('new message', {
+        username: socket.username,
+        message: data
+      });
+    }
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    socket.userId = username;
+    activeUsers.add(username);
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers,
+      activeUsers: [...activeUsers]
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+      activeUsers.delete(socket.userId);
+      
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+  
+  function replyClient(data) {
+    socket.emit('new server message', {
+        data: data
+      });
+  }
+
+  function parseCommand(data) {
+    data = data.substring(1);
+    var cmd = data.split(" ")[0].toLowerCase();
+    var args = data.split(" ").slice(1);
+    console.log('parsing: data = %s, cmd = %s, args = %s', data, cmd, args);
+    
+    if (registry.has(cmd)) {
+      var result = registry.get(cmd)(data);
+      console.log("result = %s", result);
+      
+      if(result != null) {
+        replyClient(result);
+      }
+    }
+  }
+});
+
+registerCommand("help", function(cmd) {
+    var parameters = smart_split(cmd, " ", false).slice(1);
+    console.log(parameters);
+    if (parameters.length === 0) {
+        var helpText = "Commands: ";
+
+        registry.keys.forEach(registeredCmd => {
+          helpText += "/" + registeredCmd;
+        });
+        return "Commands: /users, /chat disable|enable [message] " + helpText;
+    }
+
+    if (parameters[0].toString().toLowerCase() === "chat") {
+        //if (parameters.length === 1) {
+        //    return "Please Specify title you would like to update the User Title!";
+        //}
+        var helpContext = "Usage: /chat disable [message]" +
+                          "       /chat enable";
+        return helpContext;
+    }
+
+});
+
+registerCommand("users", function(cmd) {
+    var helpContext = "Users: " + [...activeUsers];
+    return helpContext;
+});
+
+// registerCommands(["quit", "exit", "disconnect", "logout"], function(cmd) {
+//     --numUsers;
+//     activeUsers.delete(socket.userId);
+    
+//     // echo globally that this client has left
+//     socket.broadcast.emit('user left', {
+//       username: socket.username,
+//       numUsers: numUsers
+//     });
+//   return "Disconnected.";
+// });
+
+function registerCommands([...commands], func) {
+  [...commands].forEach(cmd => {
+    console.log("registering alias " + cmd);
+    registerCommand(cmd, func);
+  });
+}
+
+function registerCommand(command, func) {
+    registry.set(command.toString().toLowerCase(), func);
+}
+
+function smart_split(input, del, empty_space) {
+    if (input.length === 0) return input;
+    var outputs = [""];
+
+    var compare = function(base, insert, position) {
+        if ((position + insert.length) > base.length) return false;
+        for (var i = 0; i < insert.length; i++) {
+            if (!(base.charAt(position + i) === insert.charAt(i))) return false;
+        }
+        return true;
+    };
+
+    var quotes = false;
+    for (var i = 0; i < input.length; i++) {
+        var char = input.charAt(i);
+        if (char === '"') {
+            quotes = !quotes;
+            continue;
+        }
+
+        if (!quotes && compare(input, del, i)) {
+            outputs.push("");
+            i += del.length - 1;
+            continue;
+        }
+
+        outputs[outputs.length - 1] += char;
+    }
+
+    if (!empty_space) {
+        for (var i = 0; i < outputs.length; i++) {
+            if (outputs[i] === "") {
+                outputs.splice(i, 1);
+            }
+        }
+    }
+
+    return outputs;
+}
